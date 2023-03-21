2338336a5424be6b001a426f6880875fbfbabc2d
CARTTIGUEANE Kévin <kevin.carttigueane@epitech.eu>
2023-03-21 17:28:19 +0100

    feat(protocol): RFC | documentation

--- include/my_teams.h
+++ include/my_teams.h
@@ -11,6 +11,10 @@
     #define SUCCESS 0
     #define FAILURE -1
 
+    #define MAX_NAME_LENGTH 32
+    #define MAX_DESCRIPTION_LENGTH 255
+    #define MAX_BODY_LENGTH 512
+
     #define handle_error(msg) \
         do {                  \
             perror(msg);      \
