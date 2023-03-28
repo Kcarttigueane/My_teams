309a1179846fd138e1bbf0b5c526c182e049993d
CARTTIGUEANE Kévin <kevin.carttigueane@epitech.eu>
2023-03-28 20:19:43 +0200

    feat(client): color input and output

--- client/src/handle_server_inputs.c
+++ client/src/handle_server_inputs.c
@@ -6,6 +6,9 @@
 */
 
 #include "../include/client.h"
+#include "../include/color.h"
+
+#define CLIENT_PROMPT "➜ "
 
 int handle_server_input(client_data_t* client, char* buffer)
 {
@@ -15,11 +18,17 @@ int handle_server_input(client_data_t* client, char* buffer)
         fprintf(stderr, "Server closed connection.\n");
         return FAILURE;
     }
-
-    printf("Received from server:\n");
-    printf("\033[0;33m");
+    printf(RED);
+    printf("---------------Received from server:----------------\n");
+    printf(YELLOW);
     printf("%s", buffer);
-    printf("\033[0m");
+    printf(RED);
+    printf("-----------End of received from server:-----------\n");
+    printf(RESET);
+    printf(CYAN);
+    printf("%s", CLIENT_PROMPT);
+    printf(RESET);
+    fflush(stdout);
 
     return SUCCESS;
 }
