762ddf454f274f71d40ee9961df0297830591f16
CARTTIGUEANE Kévin <kevin.carttigueane@epitech.eu>
2023-04-01 16:54:05 +0200

    refactor: clean redundancy code to get json key-value pair

--- client/src/events/print_users.c
+++ client/src/events/print_users.c
@@ -7,79 +7,26 @@
 
 #include "../../include/client.h"
 
-bool extract_username(char** ptr, char* username)
+void print_users(char* json_response)
 {
-    *ptr = strstr(*ptr, "\"username\": \"");
-    if (*ptr == NULL) {
-        printf("Error: username not found\n");
-        return false;
-    }
-    *ptr += strlen("\"username\": \"");
-    strncpy(username, *ptr, MAX_NAME_LENGTH - 1);
-    username[strcspn(username, "\"")] = '\0';
-    return true;
-}
-
-bool extract_user_uuid(char** ptr, char* user_uuid)
-{
-    *ptr = strstr(*ptr, "\"user_uuid\": \"");
-    if (*ptr == NULL) {
-        printf("Error: user_uuid not found\n");
-        return false;
-    }
-    *ptr += strlen("\"user_uuid\": \"");
-    strncpy(user_uuid, *ptr, MAX_UUID_STR_LEN - 1);
-    user_uuid[strcspn(user_uuid, "\"")] = '\0';
-    return true;
-}
-
-bool extract_is_logged_in(char** ptr, bool* is_logged_in)
-{
-    *ptr = strstr(*ptr, "\"is_logged_in\": ");
-    if (*ptr == NULL) {
-        printf("Error: is_logged_in not found\n");
-        return false;
-    }
-    *ptr += strlen("\"is_logged_in\": ");
-    if (strncmp(*ptr, "true", 4) == 0) {
-        *is_logged_in = true;
-    } else if (strncmp(*ptr, "false", 5) == 0) {
-        *is_logged_in = false;
-    } else {
-        printf("Error: invalid is_logged_in value\n");
-        return false;
-    }
-    return true;
-}
-
-int iterate_users(char* json_str)
-{
-    char* ptr = strstr(json_str, "\"users\": [");
+    char* ptr = strstr(json_response, "\"users\": [");
     if (ptr == NULL) {
         printf("Error: users not found\n");
-        return 1;
+        return;
     }
     ptr += strlen("\"users\": [");
     while (ptr != NULL && *ptr != ']') {
         if (*ptr == ',')
             ptr++;
-        char username[MAX_NAME_LENGTH];
-        char user_uuid[MAX_UUID_STR_LEN];
+        char user_uuid[MAX_UUID_STR_LEN] = {0};
+        char username[MAX_NAME_LENGTH] = {0};
         bool is_logged_in;
-        if (!extract_username(&ptr, username)) return 1;
-        if (!extract_user_uuid(&ptr, user_uuid)) return 1;
-        if (!extract_is_logged_in(&ptr, &is_logged_in)) return 1;
-        printf("user_uuid: %s, user_name: %s, user_status: %d\n", user_uuid,
-            username, is_logged_in);
-        ptr = strstr(ptr, ",");
-    }
-    return 0;
-}
-
-void print_users(char* json_response)
-{
-    printf("la vie est belle\n");
-    if (iterate_users(json_response) != 0) {
-        printf("Error: failed to iterate users\n");
+        if (!extract_value("user_uuid", ptr, user_uuid, sizeof(user_uuid)))
+            break;
+        if (!extract_value("username", ptr, username, sizeof(username))) break;
+        if (!extract_boolean_value("is_logged_in", ptr, &is_logged_in)) break;
+        client_print_users(user_uuid, username, is_logged_in);
+        ptr = strstr(ptr, "},");
+        if (ptr != NULL) ptr += 2;
     }
 }
--- client/src/json_get_value.c
+++ client/src/json_get_value.c
@@ -27,3 +27,43 @@ char* json_get_value(char* json_str, const char* key)
     value[value_length] = '\0';
     return value;
 }
+
+bool extract_value(const char* key, char* json_str, char* value, int max_length)
+{
+    int key_len = strlen(key);
+    char search_key[key_len + 4];
+    sprintf(search_key, "\"%s\": \"", key);
+
+    char* ptr = strstr(json_str, search_key);
+    if (ptr == NULL) {
+        printf("Error: %s not found\n", key);
+        return false;
+    }
+    ptr += strlen(search_key);
+    strncpy(value, ptr, max_length);
+    value[strcspn(value, "\"")] = '\0';
+    return true;
+}
+
+bool extract_boolean_value(const char* key, char* json_str, bool* value)
+{
+    int key_len = strlen(key);
+    char search_key[key_len + 4];
+    sprintf(search_key, "\"%s\": ", key);
+
+    char* ptr = strstr(json_str, search_key);
+    if (ptr == NULL) {
+        printf("Error: %s not found\n", key);
+        return false;
+    }
+    ptr += strlen(search_key);
+    if (strncmp(ptr, "true", 4) == 0) {
+        *value = true;
+    } else if (strncmp(ptr, "false", 5) == 0) {
+        *value = false;
+    } else {
+        printf("Error: invalid %s value\n", key);
+        return false;
+    }
+    return true;
+}
